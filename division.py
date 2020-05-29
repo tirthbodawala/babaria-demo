a2e37067f7dd24dcc4a1e338a2db4b9567b763d1
Tirth Bodawala <tirthbodawala@atyantik.com>
2020-05-30 01:06:33 +0530

    Division in python

---
+++ division.py
@@ -0,0 +1,8 @@
+# this code does literally nothing useful. Just divides
+# 5 by 2
+print(5/2)
+
+"""
+This is a multiline comment, but has some really important data to it.
+--> Never run the above code with python2 as it will not give correct answer
+"""
