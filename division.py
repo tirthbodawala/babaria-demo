4b5b36b86fc6a5df5e054cb23e5c111cdc4cd6b4
Tirth Bodawala <tirthbodawala@atyantik.com>
2020-05-30 01:29:20 +0530

    Improve formating of the code

--- division.py
+++ division.py
@@ -1,6 +1,8 @@
 # this code does literally nothing useful. Just divides
 # 5 by 2
-print(5/2)
+print('\n\n---- DIVIDING 5/2 -----')
+print('Answer: %.2f' %(5/2))  
+print('-----------------------\n\n')
 
 """
 This is a multiline comment, but has some really important data to it.
